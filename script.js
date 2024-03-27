3cfe5c3b04e6354cc0cea6b76bbbff474b94c86e
ravindrasingh7897 <ravindrasingh4632@gmail.com>
2024-03-27 18:32:06 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function bulbOn(){
+    let bulbobj = document.querySelector(".bulboff")
+    bulbobj.setAttribute("src","pic_bulbon.gif")
+}
+
+function bulbOff(){
+    let bulbobj = document.querySelector(".bulboff")
+    bulbobj.setAttribute("src","pic_bulboff.gif")
+}
